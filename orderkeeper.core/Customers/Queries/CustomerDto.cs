01bbbb3deb43d5c56cd530240d955322fcae4c83
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-24 23:08:08 +0300

    Added Customers Query and Mapper interface with Automapper and MediatR

---
+++ orderkeeper.core/Customers/Queries/CustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using orderkeeper.core.Common;
+using Orderkeeper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace orderkeeper.core.Customers
+{
+    public class CustomerDto : MapFrom<Customer>
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public string Fax { get; set; }
+    }
+}
---
+++ orderkeeper.core/Customers/Queries/GetCustomersQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace orderkeeper.core.Customers
+{
+    public class GetCustomersQuery : IRequest<GetCustomersResult>
+    {
+
+    }
+}
