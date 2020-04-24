2a286c4b2e8ab50eef4815b1f63f82c4bb0fad05
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-24 19:45:14 +0300

    Added core layer/project and initial iRepository definition

---
+++ orderkeeper.core/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orderkeeper.core.Interfaces
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        IAsyncEnumerable<T> GetAllAsync();
+        IAsyncEnumerable<T> GetByAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetSingleByAsync(Expression<Func<T, bool>> filter);
+        Task<T> CreateByAsync(T entity);
+        Task<T> UpdateByAsync(Expression<Func<T, bool>> filter,T entity);
+        Task<T> DeleteByAsync(Expression<Func<T, bool>> filter);
+    }
+
+}
