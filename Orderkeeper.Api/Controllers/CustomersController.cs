c425e335c2c50a5cdd0236c35df6062bdeaad7c2
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-25 21:16:07 +0300

    Added api and controller with method returning the list of customers

---
+++ Orderkeeper.Api/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Orderkeeper.Core.Customers;
+
+namespace Orderkeeper.Api.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ILogger<CustomersController> _logger;
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CustomerDto>> Get()
+        {
+            return await Mediator.Send(new GetCustomersQuery());
+        }
+    }
+}
---
+++ Orderkeeper.Api/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orderkeeper.Core;
+using Orderkeeper.Core.Interfaces;
+using Orderkeeper.Domain.Entities;
+using Orderkeeper.Infrastructure;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Orderkeeper.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IRepository<Customer>,CustomerRepository>();
+            services.AddOrderkeeperCore();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
