ae15b86f47d521c2cd9f6c7405636b133b6f8966
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-25 13:23:26 +0300

    Added getcustomerquery

---
+++ Orderkeeper.Core/Customers/Queries/GetCustomer/GetCustomerQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orderkeeper.Core.Customers
+{
+    public class GetCustomerQuery : IRequest<GetCustomerResult>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Orderkeeper.Core/Customers/Queries/GetCustomer/GetCustomerResult.cs
@@ -0,0 +1,12 @@
+﻿using Orderkeeper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orderkeeper.Core.Customers
+{
+    public class GetCustomerResult
+    {
+        public CustomerDto Customer { get; set; }
+    }
+}
