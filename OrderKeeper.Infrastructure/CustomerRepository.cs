bf1c9f883a84bafa5e8f34c5cb19c21decccded2
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-25 21:13:46 +0300

    Added sample customer repository and infastructure project

---
+++ OrderKeeper.Infrastructure/CustomerRepository.cs
@@ -0,0 +1,50 @@
+﻿using Orderkeeper.Core.Interfaces;
+using Orderkeeper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Orderkeeper.Infrastructure
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        public Task<Customer> CreateByAsync(Customer entity)
+        {
+            return Task.FromResult(new Customer() { Id = 1 });
+        }
+
+        public Task<Customer> DeleteByAsync(Expression<Func<Customer, bool>> filter)
+        {
+            return Task.FromResult(new Customer() { Id = 1 });
+
+        }
+
+        public async IAsyncEnumerable<Customer> GetAllAsync()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                yield return new Customer() { Id = i };
+            }
+        }
+
+        public async IAsyncEnumerable<Customer> GetByAsync(Expression<Func<Customer, bool>> filter)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                yield return new Customer() { Id = i };
+            }
+        }
+
+        public Task<Customer> GetSingleByAsync(Expression<Func<Customer, bool>> filter)
+        {
+            return Task.FromResult(new Customer() { Id = 1 });
+        }
+
+        public Task<Customer> UpdateByAsync(Expression<Func<Customer, bool>> filter, Customer entity)
+        {
+            return Task.FromResult(new Customer() { Id = 1 });
+        }
+    }
+}
