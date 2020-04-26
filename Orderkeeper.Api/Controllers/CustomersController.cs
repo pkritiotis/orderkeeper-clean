a957c74a34acd19a9db13b06700b542010519c44
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-26 19:57:33 +0300

    added all crud operations in customer controller

--- Orderkeeper.Api/Controllers/CustomersController.cs
+++ Orderkeeper.Api/Controllers/CustomersController.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Orderkeeper.Core.Customers;
+using Orderkeeper.Core.Customers.Commands.CreateCustomer;
+using Orderkeeper.Core.Customers.Commands.DeleteCustomer;
+using Orderkeeper.Core.Customers.Commands.UpdateCustomer;
 
 namespace Orderkeeper.Api.Controllers
 {
@@ -19,6 +23,30 @@ namespace Orderkeeper.Api.Controllers
         public async Task<IEnumerable<CustomerDto>> Get()
         {
             return await Mediator.Send(new GetCustomersQuery());
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<GetCustomerResult> Get(Guid Id)
+        {
+            return await Mediator.Send(new GetCustomerQuery { Id = Id });
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task Delete(Guid Id)
+        {
+            await Mediator.Send(new DeleteCustomerCommand { customerId = Id });
+        }
+
+        [HttpPut("{Id}")]
+        public async Task Delete(Guid Id, UpdatedCustomerDto updatedCustomer)
+        {
+            await Mediator.Send(new UpdateCustomerCommand { UpdatedCustomer = updatedCustomer});
+        }
+
+        [HttpPost]
+        public async Task<Guid> Create( NewCustomerDto newCustomer)
+        {
+            return await Mediator.Send(new CreateCustomerCommand { NewCustomer = newCustomer });
         }
     }
 }
