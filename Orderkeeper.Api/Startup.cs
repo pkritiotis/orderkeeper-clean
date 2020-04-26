206e2c79fa87269428009ebbde6174bf31f9f42d
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-04-26 19:58:06 +0300

    Added swashbuckle for swagger UI generation

--- Orderkeeper.Api/Startup.cs
+++ Orderkeeper.Api/Startup.cs
@@ -9,6 +9,7 @@ using Orderkeeper.Domain.Entities;
 using Orderkeeper.Infrastructure;
 using OrderKeeper.Infrastructure;
 using System.Reflection.Metadata.Ecma335;
+using Microsoft.OpenApi.Models;
 
 namespace Orderkeeper.Api
 {
@@ -28,6 +29,10 @@ namespace Orderkeeper.Api
             services.AddOrderkeeperCore();
             services.AddInfrastructure(Configuration);
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orderkeeper API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,14 @@ namespace Orderkeeper.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orderkeeper V1");
+                c.RoutePrefix = string.Empty;
+
+            });
 
             app.UseRouting();
 
