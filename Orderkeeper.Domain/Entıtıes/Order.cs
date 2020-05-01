7ee75c08ff332d8dc3c997cbaa2e2c5f4c9a6140
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-05-02 00:12:02 +0300

    removed auditing details from order entity

--- Orderkeeper.Domain/Entıtıes/Order.cs
+++ Orderkeeper.Domain/Entıtıes/Order.cs
@@ -6,9 +6,6 @@ namespace Orderkeeper.Domain.Entities
     public class Order
     {
         public Guid Id { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
-        public DateTime DateIssued { get; set; }
         public int CustomerId { get; set; }
         public double  TotalAmount { get; set; }
         public IEnumerable<OrderItem> OrderItems {get;set;}
