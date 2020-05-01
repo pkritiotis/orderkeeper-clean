cab8c321fe9080164a9dca854c0766f066cf21ab
Panayiotis Kritiotis <pkritiotis@gmail.com>
2020-05-02 00:20:42 +0300

    Fixed products queries and added dto

---
+++ Orderkeeper.Core/Products/Queries/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using Orderkeeper.Core.Common;
+using Orderkeeper.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Orderkeeper.Core.Products
+{
+    public class ProductDto : MapFrom<Product>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double DefaultPrice { get; set; }
+        public string PriceCurrency { get; set; }
+        public string DefaultUnit { get; set; }
+    }
+}
